e9787e57a150f5d02146240f73ddaa3fee6e7532
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-05-24 11:46:10 -0300

    att style

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,13 @@ let nomeOk = false
 let emailOK = false
 let assuntoOK = false
 
+
 function validaNome() {
     let txt = document.querySelector('#txtNome')
     if(nome.value.length <3){
-       txt.innerHTML = 'Nome Invalido'
-       txt.style.color = 'black'
+      
     }else {
-        txt.innerHTML = 'Nome Valido'
-        txt.style.color = '#F7E59D'
+       
         nomeOk = true
     }
 }
@@ -20,11 +19,9 @@ function validaEmail() {
     let txtEmail = document.querySelector('#txtEmail')
 
     if(email.value.indexOf('@')== -1 || email.value.indexOf('.') == -1 ) {
-        txtEmail.innerHTML = 'E-mail invalido '
-        txtEmail.style.color = 'black'
+      
     }else {
-        txtEmail.innerHTML = 'E-mail Valido'
-        txtEmail.style.color = '#F7E59D'
+       
         emailOK = true
     }
 }
@@ -32,11 +29,9 @@ function validaAssunto () {
     let txtAssunto = document.querySelector('#txtAssunto')
 
     if (assunto.value.length >= 100) {
-        txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
-        txtAssunto.style.color = 'black'
-        txtAssunto.style.display = 'block'
+       
      } else {
-        txtAssunto.style.display = 'none'
+      
         assuntoOK = true
      }
 }
